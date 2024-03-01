ca9ffd0e3ad60d8226992523f4f78a690e33c06b
mudassarali.official <mudassarali.official@gmail.com>
2024-03-01 23:03:49 +0500

    ViewEncapsulation in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'ViewEncapsulationInAngular';
